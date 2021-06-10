cf2cdbfcd7b843f57f3444e806af5edae80f6365
Husnain Javed <me@husnainjaved.com>
2021-06-10 17:27:17 +0100

    Fix MetaMask connection

--- packages/web/modules/ethereum/EthereumClient.ts
+++ packages/web/modules/ethereum/EthereumClient.ts
@@ -1,33 +1,40 @@
 import Web3 from 'web3';
 
-import { MintToken } from '@fanbase/eth-transactions';
 import { Token, Wallet } from '@fanbase/shared';
 
 export default class EthereumClient {
-  web3: Web3;
+  web3: Web3 = new Web3();
 
   static instance: EthereumClient;
 
-  constructor () {
-    this.web3 = new Web3();
-
-    if (typeof window !== 'undefined') {
-      const provider = (window as any).ethereum;
-      this.web3.setProvider(provider);
-    }
+  constructor() {
+    this.initWeb3();
 
     EthereumClient.instance = this;
   }
 
-  async getAddress (): Promise<string> {
+  async initWeb3(): Promise<void> {
+    if (typeof window !== 'undefined') {
+      let ethereum = (window as any).ethereum;
+
+      if (ethereum) {
+        ethereum.request({ method: 'eth_requestAccounts' });
+      }
+
+      this.web3.setProvider(ethereum);
+      ethereum.web3 = this.web3;
+    }
+  }
+
+  async getAddress(): Promise<string> {
     return await this.web3.eth.getCoinbase();
   }
 
-  async signMessage (message: string, address: string) {
+  async signMessage(message: string, address: string) {
     return this.web3.eth.personal.sign(message, address, '');
   }
 
-  async mintToken (
+  async mintToken(
     token: Token,
     wallet: Wallet,
     signature: string
