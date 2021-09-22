be8bea1c434865818668d531084580847699087c
Husnain Javed <husnain@luno.com>
2021-09-22 21:13:24 +0100

    Improve Ganache deployment script and add instructions to README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Fanbase
+
+### Deploy Ganache
+
+Set a different `GANACHE_NETWORK` in env variables for each development machine to avoid overriding the network config in the build artifact (as it's commited to version control).
+
+Run `scripts/deploy_ganache.sh` from root dir to start the Ganache instance and deploy contracts. It will also install Ganache, Truffle and any other dependencies specified in the contracts dir _package.json_.
+
+It is advised to also specify a valid `MNEMONIC` in env vars to persist the accounts and deployments across runs.
+
+Here are all the vars that can be explictly set:
+
+`GANACHE_PATH`
+`GANACHE_PORT`
+`GANACHE_NETWORK`
+`CONTRACTS_PATH`
+
+You may also run the script with the `-r (or --reset)` flag to delete Ganache's persisted data in `GANACHE_PATH` and re-deploy all contracts.
--- scripts/deploy_ganache.sh
+++ scripts/deploy_ganache.sh
@@ -36,24 +36,26 @@ else
   echo "Mnemonic is not set."
 fi
 
-_GANACHE_PATH=${VARIABLE:="$HOME/.ganache"}
-_LOGS_DIR="$_GANACHE_PATH/logs"
+: ${GANACHE_PATH:="$HOME/.ganache"}
+_LOGS_DIR="$GANACHE_PATH/logs"
 
 _IS_FIRST_RUN=true
-_GANACHE_PORT=8545
 
-_CONTRACTS_PATH="./packages/ethereum/contracts/solidity"
+: ${GANACHE_PORT:=8545}
+: ${GANACHE_NETWORK:=123456}
+
+: ${CONTRACTS_PATH:="./packages/ethereum/contracts/solidity"}
 
 if [ $_DO_RESET == false ];
   then
-    if [ -d $_GANACHE_PATH/data ];
+    if [ -d $GANACHE_PATH/data ];
       then
         echo "Data exists. Using existing installation."
         _IS_FIRST_RUN=false
     fi
 else
   echo "Clearing data..."
-  rm -rf $_GANACHE_PATH
+  rm -rf $GANACHE_PATH
 fi
 
 if [ $_IS_FIRST_RUN == true ] || [ ! -d $_LOGS_DIR ];
@@ -64,7 +66,7 @@ fi
 
 deploy_contracts() {
   echo "Deploying contracts..."
-  cd $_CONTRACTS_PATH
+  cd $CONTRACTS_PATH
   npm install
   npm run truffle:dev
 }
@@ -76,15 +78,17 @@ deploy_contracts_if_needed() {
   fi
 }
 
+_LOG_FILENAME="ganache-$(date +%Y-%m-%d-%H:%M:%S).log"
+
 start_ganache() {
   echo "Starting Ganache..."
 
-  echo "Logging to $_LOGS_DIR/$LOG_FILENAME"
+  echo "Logging to $_LOGS_DIR/$_LOG_FILENAME"
 
   ganache-cli \
-    -h 0.0.0.0 -p $_GANACHE_PORT \
-    -i 123456 \
-    --db "$_GANACHE_PATH/data" \
+    -h 0.0.0.0 -p $GANACHE_PORT \
+    -i $GANACHE_NETWORK \
+    --db "$GANACHE_PATH/data" \
     --accounts 10 \
     -e 100 \
     -m "$MNEMONIC" \
@@ -94,4 +98,4 @@ start_ganache() {
 {
   start_ganache &
   deploy_contracts_if_needed;
-} | tee "$_LOGS_DIR/ganache-$(date +%Y-%m-%d-%H:%M:%S).log"
+} | tee "$_LOGS_DIR/$_LOG_FILENAME"
