515fb9be19d45341c0ce43fb2308049b0a3277fe
Husnain Javed <husnain@luno.com>
2021-09-24 20:07:09 +0100

    Re-deploy contracts to Polygon Mumbai

--- packages/ethereum/contracts/solidity/truffle-config.js
+++ packages/ethereum/contracts/solidity/truffle-config.js
@@ -11,21 +11,28 @@ module.exports = {
       network_id: '*'
     },
     ropsten: {
-      provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC_TEST, process.env.PROVIDER_ROPSTEN)
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC_TEST,
+          process.env.PROVIDER_ROPSTEN
+        );
       },
       network_id: 3,
       gas: 8000000,
       gasPrice: 10000000000
     },
     mumbai: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC_TEST, process.env.PROVIDER_POLYGON_MUMBAI),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.PRIVATE_KEY_STAGING,
+          process.env.PROVIDER_POLYGON_MUMBAI
+        ),
       network_id: 80001,
-      gasPrice: 3000000000,
+      gasPrice: 2000000000,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true
-    },
+    }
   },
   compilers: {
     solc: {
