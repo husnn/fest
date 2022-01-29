89d8831b58f63891f2b675ffbc991fbe6b94d5af
Husnain Javed <husnain@luno.com>
2022-01-29 14:55:53 +0000

    postgres: Introduce migrations

--- packages/postgres/src/index.ts
+++ packages/postgres/src/index.ts
@@ -26,7 +26,7 @@ export default {
             username,
             password
           }),
-      synchronize: process.env.NODE_ENV !== 'production',
+      synchronize: process.env.NODE_ENV === 'development',
       entities: entitySchemas,
       ...(uri && {
         ssl: { rejectUnauthorized: false }
---
+++ packages/postgres/src/schemas.ts
@@ -0,0 +1,35 @@
+import CommunitySchema from './schemas/CommunitySchema';
+import InviteSchema from './schemas/InviteSchema';
+import MessageSchema from './schemas/MessageSchema';
+import NotificationSchema from './schemas/NotificationSchema';
+import OAuthSchema from './schemas/OAuthSchema';
+import PostSchema from './schemas/PostSchema';
+import ReferralSchema from './schemas/ReferralSchema';
+import RoomSchema from './schemas/RoomSchema';
+import TokenListingSchema from './schemas/TokenListingSchema';
+import TokenOfferSchema from './schemas/TokenOfferSchema';
+import TokenOwnershipSchema from './schemas/TokenOwnershipSchema';
+import TokenSchema from './schemas/TokenSchema';
+import TokenTradeSchema from './schemas/TokenTradeSchema';
+import UserSchema from './schemas/UserSchema';
+import WaitlistEntrySchema from './schemas/WaitlistEntrySchema';
+import WalletSchema from './schemas/WalletSchema';
+
+export default [
+  CommunitySchema,
+  InviteSchema,
+  MessageSchema,
+  NotificationSchema,
+  OAuthSchema,
+  PostSchema,
+  ReferralSchema,
+  RoomSchema,
+  TokenListingSchema,
+  TokenOfferSchema,
+  TokenTradeSchema,
+  TokenOwnershipSchema,
+  TokenSchema,
+  UserSchema,
+  WaitlistEntrySchema,
+  WalletSchema
+];
