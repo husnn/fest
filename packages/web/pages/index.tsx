033c882dca742006947bd49818d98f66749d3bdc
Husnain Javed <husnain@luno.com>
2022-02-22 00:14:30 +0000

    Add new landing page

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,162 +1,466 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 import { Button, Link } from '../ui';
-import React, { useEffect } from 'react';
+import head from 'next/head';
 
-import Fade from 'react-reveal/Fade';
-import Flash from 'react-reveal/Flash';
-import Head from 'next/head';
-import { getCurrentUser } from '../modules/auth/authStorage';
-import router from 'next/router';
-import styles from '../styles/Home.module.scss';
-import { useHeader } from '../modules/navigation';
+const Header = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  padding: 50px 20px;
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  z-index: 10;
 
-export default function Home() {
-  useHeader(['profile', 'login']);
+  > * {
+    font-size: 30pt;
+  }
+`;
 
-  useEffect(() => {
-    if (getCurrentUser()) router.push('/home');
-  }, []);
+const Heading = styled.h1`
+  font-size: 40pt;
+  text-align: center;
+  font-weight: 800;
+  color: #f5f5f5;
+  z-index: 10;
 
-  const currentYear = new Date().getFullYear();
+  > span {
+    font-size: inherit;
+    font-weight: inherit;
+  }
 
+  @media screen and (max-width: 500px) {
+    font-size: 30pt;
+  }
+`;
+
+const Main = styled.div`
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > * + * {
+    margin-top: 20px;
+  }
+`;
+
+const CoinScroller = styled.div`
+  width: 100%;
+  height: 150px;
+  background: url('/images/ill-pixel-coin.png') repeat-x;
+  background-size: 100px;
+  animation: coin-scroll 500000s linear infinite;
+
+  @keyframes coin-scroll {
+    0% {
+      background-position: 0 0;
+    }
+
+    100% {
+      background-position: 10000000px 0;
+    }
+  }
+`;
+
+const VideoSection = styled.div`
+  width: 100vw;
+  margin: 80px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+const FullWidthVideo = styled.video`
+  width: 100%;
+
+  @media screen and (max-width: 500px) {
+    width: 700px;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 1000px;
+  }
+`;
+
+const VideoContent = styled.div`
+  width: 80%;
+  max-width: 600px;
+  position: absolute;
+  text-align: center;
+`;
+
+const SplitSection = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  padding: 30px 0;
+
+  > div {
+    width: 100%;
+    padding: 50px;
+
+    @media screen and (max-width: 600px) {
+      padding: 20px;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    text-align: center;
+
+    div {
+      + * * {
+        margin-top: 20px;
+      }
+    }
+  }
+`;
+
+const Section = styled.div`
+  width: 100vw;
+  padding: 20px 0 50px;
+  text-align: center;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+const Title = styled.h1`
+  color: #ffffff;
+  font-size: 25pt;
+  font-family: 'Press Start 2P';
+  z-index: 10;
+`;
+
+const Description = styled.p`
+  margin-top: 30px;
+  color: #ffffff;
+  font-family: 'Courier';
+  font-size: 16pt;
+  font-weight: 100;
+  letter-spacing: 0.1em;
+
+  @media screen and (max-width: 600px) {
+    font-size: 14pt;
+  }
+`;
+
+const FeatureList = styled.ul`
+  width: 90%;
+  padding: 30px 0;
+  color: #fff;
+  text-align: center;
+  list-style: none;
+  z-index: 10;
+
+  li {
+    padding: 20px 0;
+    font-family: 'Courier';
+    font-size: 16pt;
+
+    @media screen and (max-width: 600px) {
+      font-size: 14pt;
+    }
+  }
+`;
+
+const FeatureText = styled.p`
+  color: #fff;
+  font-family: 'Courier';
+  font-size: 16pt;
+
+  @media screen and (max-width: 600px) {
+    font-size: 14pt;
+  }
+`;
+
+const GhostButton = styled.button`
+  max-height: 50px;
+  padding: 15px 30px;
+  background: none;
+  color: #fff;
+  border: 1px solid #fff;
+  cursor: pointer;
+  transition: color 0.5s;
+
+  &:hover {
+    background: #fff;
+    color: #000;
+  }
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  padding: 50px 0;
+  display: flex;
+  justify-content: center;
+  color: white;
+`;
+
+const SocialIcons = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #1a1a1a;
+  padding: 10px 20px 5px;
+  border-radius: 15px;
+
+  a {
+    cursor: pointer;
+    opacity: 0.5;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+
+  > * + * {
+    margin-left: 20px;
+  }
+`;
+
+const CircularGradient = styled.div`
+  width: 800px;
+  height: 800px;
+  position: absolute;
+  background: radial-gradient(
+    circle,
+    rgba(143, 0, 255, 1) 0%,
+    rgba(0, 0, 0, 0.32) 50%
+  );
+  opacity: 0.2;
+  z-index: 0;
+`;
+
+const HomePageV1 = () => {
   return (
-    <div className="full-width">
-      <Head>
+    <div
+      className="container"
+      css={css`
+        padding: 0;
+        top: 0;
+        background-color: #000411;
+        overflow: hidden;
+      `}
+    >
+      <head>
         <title>Fest</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.main}>
-        <h1 className={styles.mainHeading}>
+      </head>
+      <Header>
+        <div>🎉</div>
+        <div>
+          <Link
+            css={css`
+              color: #fff;
+            `}
+            href="/login"
+          >
+            Login
+          </Link>
+        </div>
+      </Header>
+      <Main>
+        <CircularGradient
+          css={css`
+            background: radial-gradient(
+              circle,
+              rgba(0, 255, 145, 1) 0%,
+              rgba(0, 0, 0, 0.32) 50%
+            );
+            top: -150px;
+            left: 0;
+          `}
+        />
+        <CircularGradient
+          css={css`
+            right: 0;
+          `}
+        />
+        <Heading
+          css={css`
+            margin-top: 80px;
+          `}
+        >
           Build your own{' '}
           <span style={{ whiteSpace: 'nowrap' }}>NFT-backed</span>
-          <span style={{ display: 'block' }}>community</span>
-        </h1>
-        <Link href="/waitlist">
-          <Button color="primary" size="small">
-            Get early access
-          </Button>
+          <span style={{ display: 'block', marginLeft: -30 }}>
+            community
+            <span
+              css={css`
+                position: absolute;
+                margin: -20px 0 0 -5px;
+                font-size: 1.3em;
+                transform: rotate(10deg);
+              `}
+            >
+              ✌️
+            </span>
+          </span>
+        </Heading>
+        <Link
+          href="/waitlist"
+          css={css`
+            z-index: 10;
+          `}
+        >
+          <Button color="primary">Get early access</Button>
         </Link>
-        <img
-          src={'images/cube-illustration-1.svg'}
-          style={{ marginTop: 30, maxWidth: 200 }}
-        />
-      </div>
-      <div className={`${styles.sectionGrey} ${styles.fullWidth}`}>
-        <span>
-          <h2 className={styles.sectionTitle}>Let your work be supported</h2>
-          <p className={styles.description}>by those who truly appreciate it</p>
-        </span>
+        <SocialIcons
+          css={css`
+            background-color: #090e1f;
+            margin-top: 120px;
+          `}
+        >
+          <Link href="https://discord.gg/dg8E4Vnm6U" target="blank">
+            <img src="/images/ic-discord.svg" height={28} />
+          </Link>
+          <Link href="https://twitter.com/festdao" target="blank">
+            <img src="/images/ic-twitter.svg" height={22} />
+          </Link>
+        </SocialIcons>
+      </Main>
+      <VideoSection>
+        <FullWidthVideo muted loop id="myVideo" autoPlay>
+          <source src="/videos/vid-neon-rectangle-1.webm" type="video/webm" />
+        </FullWidthVideo>
+        <VideoContent>
+          <Title>What is Fest?</Title>
+          <Description>
+            Fest is a platform that allows creators to build exclusive,
+            token-gated communities for their fans.
+          </Description>
+        </VideoContent>
+      </VideoSection>
+      <Section>
+        <Title>Features</Title>
+        <SplitSection>
+          <Section>
+            <img
+              style={{ width: '100%' }}
+              src={'images/share-content-illustration-1.svg'}
+            />
+          </Section>
+          <Section>
+            <FeatureText>
+              Share exclusive content with specific token holders.
+            </FeatureText>
+          </Section>
+        </SplitSection>
+        <FeatureList>
+          <li>Zero-crypto knowledge required</li>
+          <li>Turn your YouTube videos in NFTs</li>
+          <li>Custodial wallets for users</li>
+        </FeatureList>
         <Link
           href="https://www.notion.so/Lightpaper-v0-1-1563dc476d2a422e85addaadcb8c1cda"
           target="blank"
         >
-          <Button color="normal" style={{ margin: '0 auto' }} size="small">
-            Read lightpaper
+          <GhostButton>Read lightpaper</GhostButton>
+        </Link>
+      </Section>
+      <VideoSection>
+        <FullWidthVideo muted loop autoPlay>
+          <source src="/videos/vid-neon-triangles-1.webm" type="video/webm" />
+        </FullWidthVideo>
+      </VideoSection>
+      <Section>
+        <CircularGradient
+          css={css`
+            left: 0;
+          `}
+        />
+        <Title>Roadmap</Title>
+        <p
+          css={css`
+            padding: 5px 10px;
+            border-radius: 20px;
+            opacity: 0.7;
+            margin-top: 10px;
+            color: black;
+            background-color: white;
+          `}
+        >
+          Coming soon
+        </p>
+        <FeatureList>
+          <li>Fest DAO + utility token</li>
+          <li>Collection launcher</li>
+          <li>AdSense revenue share</li>
+          <li>Shopify app</li>
+          <li>Event ticketing system</li>
+          <li>Creator coins</li>
+        </FeatureList>
+      </Section>
+      <CoinScroller />
+      <Section>
+        <Title>Connect with your top fans</Title>
+        <div
+          css={css`
+            width: 600px;
+            margin-top: 30px;
+            background-color: #090e1f;
+            padding: 30px;
+            border-radius: 30px;
+
+            @media screen and (max-width: 800px) {
+              max-width: 80%;
+            }
+          `}
+        >
+          <p
+            css={css`
+              font-family: 'Courier';
+              font-size: 16pt;
+
+              @media screen and (max-width: 600px) {
+                font-size: 14pt;
+              }
+            `}
+          >
+            It&apos;s impossible to connect with everyone, prioritize those who
+            love you the most.
+          </p>
+        </div>
+        <Link
+          href="/waitlist"
+          css={css`
+            z-index: 10;
+          `}
+        >
+          <Button
+            css={css`
+              margin-top: 30px;
+            `}
+          >
+            Apply to beta
           </Button>
         </Link>
-      </div>
-      <div className={styles.section}>
-        <h2 className={styles.sectionTitleLeft}>Connect with your top fans</h2>
-        <div className={styles.splitSection} style={{ marginTop: 20 }}>
-          <Fade big bottom>
-            <div>
-              <img src={'images/mint-illustration-1.svg'} />
-            </div>
-          </Fade>
-          <Fade bottom cascade>
-            <div className={styles.featureBlock}>
-              <h3 className={styles.subheadingDescription}>
-                Launch rare collectibles
-              </h3>
-              <p>
-                You could turn your first YouTube video into a verified NFT and
-                auction it.
-              </p>
-            </div>
-          </Fade>
-          {/* <span>
-            <p style={{ display: 'block', marginTop: 20, opacity: '0.7' }}>
-              Earn lifetime royalties on each sale
-            </p>
-            <div style={{ display: 'flex', gap: 30, opacity: '0.3' }}>
-              <h2 className={styles.sectionTitle}>+10%</h2>
-              <h2 className={styles.sectionTitle}>+20%</h2>
-              <h2 className={styles.sectionTitle}>+30%</h2>
-            </div>
-          </span> */}
-        </div>
-        <div className={styles.splitSection} style={{ marginTop: 30 }}>
-          <Fade bottom cascade>
-            <div className={styles.featureBlock}>
-              <h3 className={styles.subheadingDescription}>
-                Share exclusive content
-              </h3>
-              <p>
-                Share unique content with different people based on which
-                token(s) they own.
-              </p>
-            </div>
-          </Fade>
-          <Fade big bottom>
-            <div>
-              <img
-                src={'images/share-content-illustration-1.svg'}
-                // style={{ transform: 'scaleX(-1)' }}
-              />
-            </div>
-          </Fade>
-        </div>
-      </div>
-      <div
-        className={`${styles.section} ${styles.fullWidth}`}
-        style={{ background: '#000D20' }}
-      >
-        <span>
-          <Flash fraction={1} delay={200}>
-            <h2 className={styles.sectionTitle} style={{ color: '#fff' }}>
-              Build a stronger connection
-            </h2>
-          </Flash>
-          <p className={styles.description} style={{ color: '#fff' }}>
-            It&apos;s impossible to connect with everyone,
-            <br />
-            prioritize those who love you the most.
-          </p>
-        </span>
-        {/* <Link href="/login">
-          <Button color="secondary" size="small">
-            Get started
-          </Button>
-        </Link> */}
-      </div>
-      <div className={styles.section}>
-        <Fade delay={500}>
-          <span>
-            <h3
-              className={styles.subheadingDescription}
-              style={{ opacity: 0.3 }}
-            >
-              Fest is...
-            </h3>
-            <h2 className={styles.sectionTitle} style={{ marginTop: 5 }}>
-              A home for your community
-            </h2>
-            <p className={styles.description}>
-              Create your own starting today.
-            </p>
-          </span>
-          <Link href="/login">
-            <Button color="secondary" size="small">
-              Get started
-            </Button>
-          </Link>
-        </Fade>
-      </div>
-      <div className={styles.footer}>
-        <div className={styles.footerWrapper}>
-          <p>&copy; {currentYear} Fest</p>
+      </Section>
+      <Footer>
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            > * + * {
+              margin-top: 20px;
+            }
+          `}
+        >
+          <p>&copy; {new Date().getFullYear()} Fest</p>
           <Link href="/privacy">Privacy Policy</Link>
         </div>
-      </div>
+      </Footer>
     </div>
   );
-}
+};
+
+export default HomePageV1;
---
+++ packages/web/pages/landing-old.tsx
@@ -0,0 +1,162 @@
+import { Button, Link } from '../ui';
+import React, { useEffect } from 'react';
+
+import Fade from 'react-reveal/Fade';
+import Flash from 'react-reveal/Flash';
+import Head from 'next/head';
+import { getCurrentUser } from '../modules/auth/authStorage';
+import router from 'next/router';
+import styles from '../styles/Home.module.scss';
+import { useHeader } from '../modules/navigation';
+
+export default function Home() {
+  useHeader(['profile', 'login']);
+
+  useEffect(() => {
+    if (getCurrentUser()) router.push('/home');
+  }, []);
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="full-width">
+      <Head>
+        <title>Fest</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <h1 className={styles.mainHeading}>
+          Build your own{' '}
+          <span style={{ whiteSpace: 'nowrap' }}>NFT-backed</span>
+          <span style={{ display: 'block' }}>community</span>
+        </h1>
+        <Link href="/waitlist">
+          <Button color="primary" size="small">
+            Get early access
+          </Button>
+        </Link>
+        <img
+          src={'images/cube-illustration-1.svg'}
+          style={{ marginTop: 30, maxWidth: 200 }}
+        />
+      </div>
+      <div className={`${styles.sectionGrey} ${styles.fullWidth}`}>
+        <span>
+          <h2 className={styles.sectionTitle}>Let your work be supported</h2>
+          <p className={styles.description}>by those who truly appreciate it</p>
+        </span>
+        <Link
+          href="https://www.notion.so/Lightpaper-v0-1-1563dc476d2a422e85addaadcb8c1cda"
+          target="blank"
+        >
+          <Button color="normal" style={{ margin: '0 auto' }} size="small">
+            Read lightpaper
+          </Button>
+        </Link>
+      </div>
+      <div className={styles.section}>
+        <h2 className={styles.sectionTitleLeft}>Connect with your top fans</h2>
+        <div className={styles.splitSection} style={{ marginTop: 20 }}>
+          <Fade big bottom>
+            <div>
+              <img src={'images/mint-illustration-1.svg'} />
+            </div>
+          </Fade>
+          <Fade bottom cascade>
+            <div className={styles.featureBlock}>
+              <h3 className={styles.subheadingDescription}>
+                Launch rare collectibles
+              </h3>
+              <p>
+                You could turn your first YouTube video into a verified NFT and
+                auction it.
+              </p>
+            </div>
+          </Fade>
+          {/* <span>
+            <p style={{ display: 'block', marginTop: 20, opacity: '0.7' }}>
+              Earn lifetime royalties on each sale
+            </p>
+            <div style={{ display: 'flex', gap: 30, opacity: '0.3' }}>
+              <h2 className={styles.sectionTitle}>+10%</h2>
+              <h2 className={styles.sectionTitle}>+20%</h2>
+              <h2 className={styles.sectionTitle}>+30%</h2>
+            </div>
+          </span> */}
+        </div>
+        <div className={styles.splitSection} style={{ marginTop: 30 }}>
+          <Fade bottom cascade>
+            <div className={styles.featureBlock}>
+              <h3 className={styles.subheadingDescription}>
+                Share exclusive content
+              </h3>
+              <p>
+                Share unique content with different people based on which
+                token(s) they own.
+              </p>
+            </div>
+          </Fade>
+          <Fade big bottom>
+            <div>
+              <img
+                src={'images/share-content-illustration-1.svg'}
+                // style={{ transform: 'scaleX(-1)' }}
+              />
+            </div>
+          </Fade>
+        </div>
+      </div>
+      <div
+        className={`${styles.section} ${styles.fullWidth}`}
+        style={{ background: '#000D20' }}
+      >
+        <span>
+          <Flash fraction={1} delay={200}>
+            <h2 className={styles.sectionTitle} style={{ color: '#fff' }}>
+              Build a stronger connection
+            </h2>
+          </Flash>
+          <p className={styles.description} style={{ color: '#fff' }}>
+            It&apos;s impossible to connect with everyone,
+            <br />
+            prioritize those who love you the most.
+          </p>
+        </span>
+        {/* <Link href="/login">
+          <Button color="secondary" size="small">
+            Get started
+          </Button>
+        </Link> */}
+      </div>
+      <div className={styles.section}>
+        <Fade delay={500}>
+          <span>
+            <h3
+              className={styles.subheadingDescription}
+              style={{ opacity: 0.3 }}
+            >
+              Fest is...
+            </h3>
+            <h2 className={styles.sectionTitle} style={{ marginTop: 5 }}>
+              A home for your community
+            </h2>
+            <p className={styles.description}>
+              Create your own starting today.
+            </p>
+          </span>
+          <Link href="/login">
+            <Button color="secondary" size="small">
+              Get started
+            </Button>
+          </Link>
+        </Fade>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footerWrapper}>
+          <p>&copy; {currentYear} Fest</p>
+          <Link href="/privacy">Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
