bb7236d7957afa5db691973daa1ef510f439d589
Husnain Javed <husnain@luno.com>
2022-02-24 21:21:43 +0000

    api: Rate limit test funds request

--- packages/api/src/routes/internal.routes.ts
+++ packages/api/src/routes/internal.routes.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response, Router } from 'express';
-
 import InternalController from '../controllers/InternalController';
 import authMiddleware from '../middleware/authMiddleware';
+import { getRateLimiter } from '../middleware/rateLimiting';
 
 export default function init(internalController: InternalController): Router {
   const router = Router();
 
   router.post(
     '/test-funds',
+    getRateLimiter('requestTestFunds', { max: 5, windowInMins: 60 }),
     authMiddleware,
     (req: Request, res: Response, next: NextFunction) =>
       internalController.requestTestFunds(req, res, next)
