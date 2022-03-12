e495b80b4d3e5634fb2805c357a622974accb3b7
Husnain Javed <husnain@luno.com>
2022-03-12 14:08:07 +0000

    eth-contracts: Update migration and tsconfig

--- packages/ethereum/contracts/solidity/migrations/1_initial_migration.js
+++ packages/ethereum/contracts/solidity/migrations/1_initial_migration.js
@@ -1,6 +1,5 @@
 const Fest = artifacts.require("Fest");
-const MarketV1 = artifacts.require("MarketV1");
-const MarketWalletV1 = artifacts.require("MarketWalletV1");
+const ListingMarketV1 = artifacts.require("ListingMarketV1");
 const Migrations = artifacts.require("Migrations");
 const TokenV1 = artifacts.require("TokenV1");
 
@@ -10,7 +9,5 @@ module.exports = function (deployer) {
   deployer.deploy(Fest);
   deployer.deploy(TokenV1);
 
-  deployer.deploy(MarketWalletV1).then((instance) => {
-    deployer.deploy(MarketV1, instance.address);
-  });
+  deployer.deploy(ListingMarketV1);
 };
