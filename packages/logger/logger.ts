287c904ddac97dc5bba7154f39b929b4b7660306
Husnain Javed <husnain@luno.com>
2022-07-03 20:25:45 +0100

    logger: Fix logging for Heroku

--- packages/logger/logger.ts
+++ packages/logger/logger.ts
@@ -9,6 +9,8 @@ import { getContext } from './context';
 
 let packageName = process.env.npm_package_name;
 
+export const setupLogger = (package_: string) => (packageName = package_);
+
 const ctx = getContext();
 
 const logFormat = (info: winston.LogEntry) => {
@@ -22,67 +24,84 @@ const logFormat = (info: winston.LogEntry) => {
   };
 };
 
-const fileFormat = winston.format.combine(
+const jsonFormat = winston.format.combine(
   winston.format.timestamp(),
   winston.format.printf((info) => JSON.stringify(logFormat(info), null, 2))
 );
 
-const consoleLogFormat = winston.format.combine(
-  winston.format.colorize({ all: true }),
-  winston.format.printf((info) => `[${info.level}] ${info.message}`)
-);
-
-export const setupLogger = (package_: string) => (packageName = package_);
-
-const rotationFileConfig = {
-  format: fileFormat,
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d'
-};
-
-export const logger = winston.createLogger({
-  exitOnError: false
-});
+let loggerOpts: winston.LoggerOptions;
 
 if (process.env.NODE_ENV === 'production') {
   const startTime = new Date().toISOString();
 
-  logger.add(
-    new WinstonCloudwatch({
-      logGroupName: 'api-production',
-      logStreamName: function () {
-        const date = new Date().toISOString().split('T')[0];
-        return (
-          date + '-' + crypto.createHash('md5').update(startTime).digest('hex')
-        );
-      },
-      messageFormatter: (info) => JSON.stringify(logFormat(info))
-    })
-  );
+  loggerOpts = {
+    transports: [
+      new WinstonCloudwatch({
+        logGroupName: 'api-production',
+        logStreamName: function () {
+          const date = new Date().toISOString().split('T')[0];
+          return (
+            date +
+            '-' +
+            crypto.createHash('md5').update(startTime).digest('hex')
+          );
+        },
+        messageFormatter: (info) => JSON.stringify(logFormat(info))
+      })
+    ]
+  };
+} else if (process.env.HEROKU) {
+  loggerOpts = {
+    transports: [
+      new winston.transports.Console({
+        level: 'info',
+        format: jsonFormat
+      }),
+      new winston.transports.Console({
+        level: 'error',
+        format: jsonFormat,
+        handleExceptions: true
+      })
+    ]
+  };
 } else {
-  logger.add(
-    new winston.transports.DailyRotateFile({
-      ...rotationFileConfig,
-      filename: `${appRoot}/logs/info-%DATE%.log`,
-      level: 'info'
-    })
-  );
-  logger.add(
-    new winston.transports.DailyRotateFile({
-      ...rotationFileConfig,
-      filename: `${appRoot}/logs/error-%DATE%.log`,
-      level: 'error',
-      handleExceptions: true
-    })
-  );
+  const rotationFileConfig = {
+    format: jsonFormat,
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+  };
+
+  loggerOpts = {
+    transports: [
+      new winston.transports.DailyRotateFile({
+        ...rotationFileConfig,
+        filename: `${appRoot}/logs/info-%DATE%.log`,
+        level: 'info'
+      }),
+      new winston.transports.DailyRotateFile({
+        ...rotationFileConfig,
+        filename: `${appRoot}/logs/error-%DATE%.log`,
+        level: 'error',
+        handleExceptions: true
+      })
+    ]
+  };
 }
+
+const logger = winston.createLogger({
+  ...loggerOpts,
+  exitOnError: false
+});
 
 if (process.env.NODE_ENV === 'development') {
   logger.add(
     new winston.transports.Console({
       level: 'debug',
-      format: consoleLogFormat,
+      format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        winston.format.printf((info) => `[${info.level}] ${info.message}`)
+      ),
       handleExceptions: true
     })
   );
