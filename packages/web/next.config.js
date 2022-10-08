341c261f6f456558e1b7700a7cb8cca8adaf34b8
Husnain Javed <husnain@luno.com>
2022-10-08 02:05:38 +0100

    web: Prevent search engine indexing in preview environments

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -25,5 +25,20 @@ module.exports = withTM({
     );
 
     return config;
-  }
+  },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 });
